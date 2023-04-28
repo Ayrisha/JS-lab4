b43e8c231f780e3553fa6b3df36705852fa822ce
Ayrisha <arina1357929@gmail.com>
2023-04-28 16:06:13 +0300

    lab change text color

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").addEventListener('click', function (){
+    text.style.color = 'lightblue';
+});
+
+document.getElementById("button2").addEventListener('click', function (){
+    text.style.color = 'lightcoral';
+});
